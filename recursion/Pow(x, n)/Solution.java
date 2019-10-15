8ef8881a72ce571804ef3039a0995b7c33e14b2c
nomorejikann <275841497@qq.com>
2019-10-15 20:25:04 +0800

    Create Solution.java

---
+++ recursion/Pow(x, n)/Solution.java
@@ -0,0 +1,24 @@
+    /**
+     * 实现 pow(x, n) ，即计算 x 的 n 次幂函数。
+     * @param x
+     * @param n
+     * @return
+     */
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long i = N; i > 0; i /= 2) {
+            if ((i % 2) == 1) {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+};
