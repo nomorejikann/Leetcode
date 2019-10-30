6fca5d62f72a3a72dbd4ef3f882a6edec6a85b6b
nomorejikann <275841497@qq.com>
2019-10-30 20:25:38 +0800

    Create MyStack.java

---
+++ design/用队列实现栈/MyStack.java
@@ -0,0 +1,56 @@
+/**
+ * 使用队列实现栈的下列操作：
+
+push(x) -- 元素 x 入栈
+pop() -- 移除栈顶元素
+top() -- 获取栈顶元素
+empty() -- 返回栈是否为空
+
+ *
+ */
+class MyStack {
+	
+	Queue<Integer> queue1 = null;
+	Queue<Integer> queue2 = null; 
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        queue1 = new LinkedList<Integer>();
+        queue2 = new LinkedList<Integer>();
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue2.add(x);
+        while(queue1.size() != 0) {
+        	queue2.add(queue1.remove());
+        }
+        Queue<Integer> temp = queue1;
+        queue1 = queue2;
+        queue2 = temp;
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return queue1.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return queue1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
