2b3176169eb0ba7930eedc753de8300862961019
nomorejikann <275841497@qq.com>
2019-11-15 20:11:47 +0800

    Update Solution.java

--- tree/中序遍历二叉树/Solution.java
+++ tree/中序遍历二叉树/Solution.java
@@ -9,7 +9,7 @@
  */
 class Solution {
      private List<Integer> answerList = new ArrayList<Integer>();
-	  //左->中->右
+	  //左->根->右
     public List<Integer> inorderTraversal(TreeNode root) {
         if(root == null){
 			return answerList;
