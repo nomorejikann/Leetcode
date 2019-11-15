37309e5ef72d9a0110152d845f2aebe3b6323c7d
nomorejikann <275841497@qq.com>
2019-11-15 20:11:11 +0800

    Create Solution.java

---
+++ tree/中序遍历二叉树/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+     private List<Integer> answerList = new ArrayList<Integer>();
+	  //左->中->右
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null){
+			return answerList;
+		}
+			
+		
+		inorderTraversal(root.left);
+		answerList.add(root.val);
+		inorderTraversal(root.right);
+		return answerList;
+    }
+}
