404b5111d2e34d4a0bd43c2c391f96f1f73edbdf
nomorejikann <275841497@qq.com>
2019-12-22 19:48:38 +0800

    Create Solution.java

---
+++ tree/二叉树的所有路径/Solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ */
+class Solution {
+    public void construct_paths(TreeNode root, String path, LinkedList<String> paths) {
+        if (root != null) {
+            path += Integer.toString(root.val);
+            if ((root.left == null) && (root.right == null))  // 当前节点是叶子节点
+                paths.add(path);  // 把路径加入到答案中
+            else {
+                path += "->";  // 当前节点不是叶子节点，继续递归遍历
+                construct_paths(root.left, path, paths);
+                construct_paths(root.right, path, paths);
+            }
+        }
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        LinkedList<String> paths = new LinkedList();
+        construct_paths(root, "", paths);
+        return paths;
+    }
+
+}
