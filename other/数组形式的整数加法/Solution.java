15711d4d312c18519a82da13289d24aa6497833b
nomorejikann <275841497@qq.com>
2019-12-24 23:07:55 +0800

    Create Solution.java

---
+++ other/数组形式的整数加法/Solution.java
@@ -0,0 +1,23 @@
+/**
+对于非负整数 X 而言，X 的数组形式是每位数字按从左到右的顺序形成的数组。例如，如果 X = 1231，那么其数组形式为 [1,2,3,1]。
+给定非负整数 X 的数组形式 A，返回整数 X+K 的数组形式。
+*/
+class Solution {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        int N = A.length;
+        int cur = K;
+        List<Integer> ans = new ArrayList();
+
+        int i = N;
+        while (--i >= 0 || cur > 0) {
+            if (i >= 0)
+                cur += A[i];
+            ans.add(cur % 10);
+            cur /= 10;
+        }
+
+        Collections.reverse(ans);
+        return ans;
+    }
+
+}
