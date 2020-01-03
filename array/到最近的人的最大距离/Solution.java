017db0c1a676bd7b02a8c4e38170b35a641221d0
nomorejikann <275841497@qq.com>
2020-01-03 17:03:30 +0800

    Create Solution.java

---
+++ array/到最近的人的最大距离/Solution.java
@@ -0,0 +1,28 @@
+/**
+在一排座位（ seats）中，1 代表有人坐在座位上，0 代表座位上是空的。
+至少有一个空座位，且至少有一人坐在座位上。
+亚历克斯希望坐在一个能够使他与离他最近的人之间的距离达到最大化的座位上。
+返回他到离他最近的人的最大距离。
+*/
+class Solution {
+    public int maxDistToClosest(int[] seats) {
+        int N = seats.length;
+        int prev = -1, future = 0;
+        int ans = 0;
+
+        for (int i = 0; i < N; ++i) {
+            if (seats[i] == 1) {
+                prev = i;
+            } else {
+                while (future < N && seats[future] == 0 || future < i)
+                    future++;
+
+                int left = prev == -1 ? N : i - prev;
+                int right = future == N ? N : future - i;
+                ans = Math.max(ans, Math.min(left, right));
+            }
+        }
+
+        return ans;
+    }
+}
