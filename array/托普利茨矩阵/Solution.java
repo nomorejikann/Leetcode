9c9df33a26650698301bc7e0e7511502309eba84
nomorejikann <275841497@qq.com>
2020-01-11 17:26:39 +0800

    Update Solution.java

--- array/托普利茨矩阵/Solution.java
+++ array/托普利茨矩阵/Solution.java
@@ -4,15 +4,10 @@
 */
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        Map<Integer, Integer> groups = new HashMap();
-        for (int r = 0; r < matrix.length; ++r) {
-            for (int c = 0; c < matrix[0].length; ++c) {
-                if (!groups.containsKey(r-c))
-                    groups.put(r-c, matrix[r][c]);
-                else if (groups.get(r-c) != matrix[r][c])
+        for (int r = 0; r < matrix.length; ++r)
+            for (int c = 0; c < matrix[0].length; ++c)
+                if (r > 0 && c > 0 && matrix[r-1][c-1] != matrix[r][c])
                     return false;
-            }
-        }
         return true;
     }
 }
