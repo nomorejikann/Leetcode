2b0a5470b5d3832253ee17b706ca902254559b9b
nomorejikann <275841497@qq.com>
2020-01-13 16:08:16 +0800

    Create Solution.java

---
+++ list/链表的中间结点/Solution.java
@@ -0,0 +1,22 @@
+/**
+给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+如果有两个中间结点，则返回第二个中间结点。
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
