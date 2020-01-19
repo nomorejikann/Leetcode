d4736afffb144128f81cc9a2fb609bc5bdb96e81
nomorejikann <275841497@qq.com>
2020-01-19 20:33:48 +0800

    Create Solution.java

---
+++ list/二进制链表转整数/Solution.java
@@ -0,0 +1,25 @@
+/**
+给你一个单链表的引用结点 head。链表中每个结点的值不是 0 就是 1。已知此链表是一个整数数字的二进制表示形式。
+
+请你返回该链表所表示数字的 十进制值 。
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        if (head == null)
+			return 0;
+		int ans = 0;
+		while (head != null) {
+			ans = (ans << 1) + head.val;
+			head = head.next;
+		}
+		return ans;
+    }
+}
