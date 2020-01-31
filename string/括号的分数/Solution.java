c8ee7ce42dc4a971a9ecbdf2ec01c765fcc229ec
nomorejikann <275841497@qq.com>
2020-01-31 20:36:53 +0800

    Create Solution.java

---
+++ string/括号的分数/Solution.java
@@ -0,0 +1,24 @@
+/**
+给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
+
+() 得 1 分。
+AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
+(A) 得 2 * A 分，其中 A 是平衡括号字符串。
+*/
+class Solution {
+    public int scoreOfParentheses(String S) {
+        int ans = 0, bal = 0;
+        for (int i = 0; i < S.length(); ++i) {
+            if (S.charAt(i) == '(') {
+                bal++;
+            } else {
+                bal--;
+                if (S.charAt(i-1) == '(')
+                    ans += 1 << bal;
+            }
+        }
+
+        return ans;
+
+    }
+}
