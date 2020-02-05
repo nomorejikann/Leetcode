10cbbf248beb640e9fc412d578fc8eda6ce95342
nomorejikann <275841497@qq.com>
2020-02-05 19:32:53 +0800

    Create Solution.java

---
+++ stack/棒球比赛/Solution.java
@@ -0,0 +1,44 @@
+/**
+你现在是棒球比赛记录员。
+给定一个字符串列表，每个字符串可以是以下四种类型之一：
+1.整数（一轮的得分）：直接表示您在本轮中获得的积分数。
+2. "+"（一轮的得分）：表示本轮获得的得分是前两轮有效 回合得分的总和。
+3. "D"（一轮的得分）：表示本轮获得的得分是前一轮有效 回合得分的两倍。
+4. "C"（一个操作，这不是一个回合的分数）：表示您获得的最后一个有效 回合的分数是无效的，应该被移除。
+
+每一轮的操作都是永久性的，可能会对前一轮和后一轮产生影响。
+你需要返回你在所有回合中得分的总和。
+*/
+class Solution {
+    public int calPoints(String[] ops) {
+        int i= 0,res= 0;
+        int[] arr = new int[ops.length];
+        for(String s:ops){
+            switch(s){
+                case "+":{
+                    arr[i] = arr[i-1]+arr[i-2];
+                    i++;
+                    break;
+                }
+                case "D":{
+                    arr[i] =2*arr[i-1];
+                    i++;
+                    break;
+                }
+                case "C":{
+                    arr[i-1] = 0;
+                    i--;
+                    break;
+                }
+                default:{
+                    arr[i] = Integer.parseInt(s);
+                    i++;
+                }
+
+            }
+        }
+        for(int a :arr)
+            res +=a;
+        return res;
+    }
+}
