61999441ea9229fc2dffca61e550531ab2d09784
nomorejikann <275841497@qq.com>
2020-02-11 20:08:56 +0800

    Create Solution.java

---
+++ tree/找树左下角的值/Solution.java
@@ -0,0 +1,24 @@
+/**
+给定一个二叉树，在树的最后一行找到最左边的值。
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            root = queue.poll();
+            if (root.right != null) queue.offer(root.right);
+            if (root.left != null) queue.offer(root.left);
+        }
+        return root.val;
+    }
+}
