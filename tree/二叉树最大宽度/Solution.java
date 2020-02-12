fa109bc81b569a38da5c218bb107b875c1f1e4c2
nomorejikann <275841497@qq.com>
2020-02-12 20:37:41 +0800

    Create Solution.java

---
+++ tree/二叉树最大宽度/Solution.java
@@ -0,0 +1,32 @@
+/**
+给定一个二叉树，编写一个函数来获取这个树的最大宽度。树的宽度是所有层中的最大宽度。这个二叉树与满二叉树（full binary tree）结构相同，但一些节点为空。
+每一层的宽度被定义为两个端点（该层最左和最右的非空节点，两端点间的null节点也计入长度）之间的长度。
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int ans;
+    Map<Integer, Integer> left;
+    public int widthOfBinaryTree(TreeNode root) {
+        ans = 0;
+        left = new HashMap();
+        dfs(root, 0, 0);
+        return ans;
+    }
+    public void dfs(TreeNode root, int depth, int pos) {
+        if (root == null) return;
+        left.computeIfAbsent(depth, x-> pos);
+        ans = Math.max(ans, pos - left.get(depth) + 1);
+        dfs(root.left, depth + 1, 2 * pos);
+        dfs(root.right, depth + 1, 2 * pos + 1);
+    }
+
+
+}
