8f018d1be4e4fdf6cdea63b4f94c469b5e0269bb
nomorejikann <275841497@qq.com>
2020-02-22 20:02:18 +0800

    Create Solution.java

---
+++ string/推多米诺/Solution.java
@@ -0,0 +1,49 @@
+/**
+一行中有 N 张多米诺骨牌，我们将每张多米诺骨牌垂直竖立。
+
+在开始时，我们同时把一些多米诺骨牌向左或向右推。
+
+
+
+每过一秒，倒向左边的多米诺骨牌会推动其左侧相邻的多米诺骨牌。
+
+同样地，倒向右边的多米诺骨牌也会推动竖立在其右侧的相邻多米诺骨牌。
+
+如果同时有多米诺骨牌落在一张垂直竖立的多米诺骨牌的两边，由于受力平衡， 该骨牌仍然保持不变。
+
+就这个问题而言，我们会认为正在下降的多米诺骨牌不会对其它正在下降或已经下降的多米诺骨牌施加额外的力。
+
+给定表示初始状态的字符串 "S" 。如果第 i 张多米诺骨牌被推向左边，则 S[i] = 'L'；如果第 i 张多米诺骨牌被推向右边，则 S[i] = 'R'；如果第 i 张多米诺骨牌没有被推动，则 S[i] = '.'。
+
+返回表示最终状态的字符串。
+*/
+class Solution {
+    public String pushDominoes(String S) {
+        char[] A = S.toCharArray();
+        int N = A.length;
+        int[] forces = new int[N];
+
+        // Populate forces going from left to right
+        int force = 0;
+        for (int i = 0; i < N; ++i) {
+            if (A[i] == 'R') force = N;
+            else if (A[i] == 'L') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] += force;
+        }
+
+        // Populate forces going from right to left
+        force = 0;
+        for (int i = N-1; i >= 0; --i) {
+            if (A[i] == 'L') force = N;
+            else if (A[i] == 'R') force = 0;
+            else force = Math.max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        StringBuilder ans = new StringBuilder();
+        for (int f: forces)
+            ans.append(f > 0 ? 'R' : f < 0 ? 'L' : '.');
+        return ans.toString();
+    }
+}
