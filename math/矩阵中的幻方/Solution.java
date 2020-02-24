16070f273cdf223f5408061c8374d2f428b14a16
nomorejikann <275841497@qq.com>
2020-02-24 20:25:04 +0800

    Create Solution.java

---
+++ math/矩阵中的幻方/Solution.java
@@ -0,0 +1,39 @@
+/**
+3 x 3 的幻方是一个填充有从 1 到 9 的不同数字的 3 x 3 矩阵，其中每行，每列以及两条对角线上的各数之和都相等。
+
+给定一个由整数组成的 grid，其中有多少个 3 × 3 的 “幻方” 子矩阵？（每个子矩阵都是连续的）。
+*/
+class Solution {
+    public int numMagicSquaresInside(int[][] grid) {
+        int R = grid.length, C = grid[0].length;
+        int ans = 0;
+        for (int r = 0; r < R-2; ++r)
+            for (int c = 0; c < C-2; ++c) {
+                if (grid[r+1][c+1] != 5) continue;  // optional skip
+                if (magic(grid[r][c], grid[r][c+1], grid[r][c+2],
+                          grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                          grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2]))
+                    ans++;
+            }
+
+        return ans;
+    }
+
+    public boolean magic(int... vals) {
+        int[] count = new int[16];
+        for (int v: vals) count[v]++;
+        for (int v = 1; v <= 9; ++v)
+            if (count[v] != 1)
+                return false;
+
+        return (vals[0] + vals[1] + vals[2] == 15 &&
+                vals[3] + vals[4] + vals[5] == 15 &&
+                vals[6] + vals[7] + vals[8] == 15 &&
+                vals[0] + vals[3] + vals[6] == 15 &&
+                vals[1] + vals[4] + vals[7] == 15 &&
+                vals[2] + vals[5] + vals[8] == 15 &&
+                vals[0] + vals[4] + vals[8] == 15 &&
+                vals[2] + vals[4] + vals[6] == 15);
+    }
+}
+
