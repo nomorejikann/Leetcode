609bf6e69909f3482e59ba1d523ad350acdce7d1
nomorejikann <275841497@qq.com>
2020-03-01 18:16:24 +0800

    Create Solution.java

---
+++ other/字母移位/Solution.java
@@ -0,0 +1,27 @@
+/**
+有一个由小写字母组成的字符串 S，和一个整数数组 shifts。
+
+我们将字母表中的下一个字母称为原字母的 移位（由于字母表是环绕的， 'z' 将会变成 'a'）。
+
+例如·，shift('a') = 'b'， shift('t') = 'u',， 以及 shift('z') = 'a'。
+
+对于每个 shifts[i] = x ， 我们会将 S 中的前 i+1 个字母移位 x 次。
+
+返回将所有这些移位都应用到 S 后最终得到的字符串。
+*/
+class Solution {
+    public String shiftingLetters(String S, int[] shifts) {
+        StringBuilder ans = new StringBuilder();
+        int X = 0;
+        for (int shift: shifts)
+            X = (X + shift) % 26;
+
+        for (int i = 0; i < S.length(); ++i) {
+            int index = S.charAt(i) - 'a';
+            ans.append((char) ((index + X) % 26 + 97));
+            X = Math.floorMod(X - shifts[i], 26);
+        }
+
+        return ans.toString();
+    }
+}
