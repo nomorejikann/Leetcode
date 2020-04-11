bf292b843c2153c8b13ad69d5b00ae7129c2694d
nomorejikann <275841497@qq.com>
2020-04-11 23:17:24 +0800

    Create Solution.java

---
+++ array/单调数列/Solution.java
@@ -0,0 +1,24 @@
+/**
+如果数组是单调递增或单调递减的，那么它是单调的。
+
+如果对于所有 i <= j，A[i] <= A[j]，那么数组 A 是单调递增的。 如果对于所有 i <= j，A[i]> = A[j]，那么数组 A 是单调递减的。
+
+当给定的数组 A 是单调数组时返回 true，否则返回 false。
+*/
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        return increasing(A) || decreasing(A);
+    }
+
+    public boolean increasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] > A[i+1]) return false;
+        return true;
+    }
+
+    public boolean decreasing(int[] A) {
+        for (int i = 0; i < A.length - 1; ++i)
+            if (A[i] < A[i+1]) return false;
+        return true;
+    }
+}
