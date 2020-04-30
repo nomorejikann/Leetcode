4653a127bf49df6586e07d58d8e2cab927550d16
nomorejikann <275841497@qq.com>
2020-04-30 13:25:45 +0800

    Create Solution.java

---
+++ other/快乐数/Solution.java
@@ -0,0 +1,33 @@
+/**
+编写一个算法来判断一个数 n 是不是快乐数。
+
+「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+
+如果 n 是快乐数就返回 True ；不是，则返回 False 。
+
+*/
+class Solution {
+    public boolean isHappy(int n) {
+        List<Integer> list = new ArrayList<Integer>();
+        //4必定不是快乐数
+        while(n!=4) {
+        	//按照位数添加
+        	while(n!=0) {
+        		list.add(n%10);
+        		n/=10;
+        	}
+        	int temp = 0;
+        	for(int i:list) {
+        		temp+=i*i;
+        	}
+        	if(temp==1) {
+        		return true;
+            //下一轮
+        	}else {
+        		list.clear();
+        		n = temp;
+        	}
+        }
+		return false;
+    }
+}
