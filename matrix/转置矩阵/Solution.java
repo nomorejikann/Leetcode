de218f52f7da9e6e00e9e44e79f01f585974a638
nomorejikann <275841497@qq.com>
2020-05-22 20:24:04 +0800

    Create Solution.java

---
+++ matrix/转置矩阵/Solution.java
@@ -0,0 +1,16 @@
+/**
+给定一个矩阵 A， 返回 A 的转置矩阵。
+
+矩阵的转置是指将矩阵的主对角线翻转，交换矩阵的行索引与列索引。
+*/
+class Solution {
+    public int[][] transpose(int[][] A) {
+        int R = A.length, C = A[0].length;
+        int[][] ans = new int[C][R];
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                ans[c][r] = A[r][c];
+            }
+        return ans;
+    }
+}
