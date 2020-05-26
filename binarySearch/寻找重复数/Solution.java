1db147e21cb8724991a8d32d74e86d29615848ff
nomorejikann <275841497@qq.com>
2020-05-26 20:18:25 +0800

    Update Solution.java

--- binarySearch/寻找重复数/Solution.java
+++ binarySearch/寻找重复数/Solution.java
@@ -1,7 +1,7 @@
 /**
 给定一个包含 n + 1 个整数的数组 nums，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。
 
-*
+*/
 class Solution {
     public int findDuplicate(int[] nums) {
         int slow = nums[0];
