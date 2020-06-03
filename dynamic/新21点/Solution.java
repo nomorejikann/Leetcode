0456e303b8939bb0a646164965ec47a0a4be051f
nomorejikann <275841497@qq.com>
2020-06-03 20:27:53 +0800

    Create Solution.java

---
+++ dynamic/新21点/Solution.java
@@ -0,0 +1,24 @@
+/**
+
+爱丽丝参与一个大致基于纸牌游戏 “21点” 规则的游戏，描述如下：
+
+爱丽丝以 0 分开始，并在她的得分少于 K 分时抽取数字。 抽取时，她从 [1, W] 的范围中随机获得一个整数作为分数进行累计，其中 W 是整数。 每次抽取都是独立的，其结果具有相同的概率。
+
+当爱丽丝获得不少于 K 分时，她就停止抽取数字。 爱丽丝的分数不超过 N 的概率是多少？
+*/
+class Solution {
+    public double new21Game(int N, int K, int W) {
+        double[] dp = new double[N + W + 1];
+        // dp[x] = the answer when Alice has x points
+        for (int k = K; k <= N; ++k)
+            dp[k] = 1.0;
+
+        double S = Math.min(N - K + 1, W);
+        // S = dp[k+1] + dp[k+2] + ... + dp[k+W]
+        for (int k = K - 1; k >= 0; --k) {
+            dp[k] = S / W;
+            S += dp[k] - dp[k + W];
+        }
+        return dp[0];
+    }
+}
